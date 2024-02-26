922f4f6b9a637e0ce232d8d09e95afa3722dfc1d
Yee <873720581@qq.com>
2024-02-26 16:03:12 +0800

    上传临时目录文件

---
+++ cmake/muduo.cmake
@@ -0,0 +1,9 @@
+include(fetchcontent)
+
+fetchcontent_declare(muduo
+  GIT_REPOSITORY https://github.com/chenshuo/muduo.git
+  GIT_TAG v2.0.3
+  SOURCE_DIR ${CMAKE_CURRENT_SOURCE_DIR}/thirdparty/muduo
+)
+
+fetchcontent_makeavailable(muduo)
